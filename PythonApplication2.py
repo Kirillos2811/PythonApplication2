541be9f0f4409cf48ae29b32bed2597fa943bebd
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-14 03:07:08 +0300

    Lesson 16 task 2

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,20 +1,36 @@
-class Transport:
+class Turtle:
 
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
  
+    def go_up(self):
+        self.y += self.s
 
-    def seating_capacity(self, capacity):
-        return f"Вместимость одного автобуса {self.name}  {capacity} пассажиров"
+    def go_down(self):
+        self.y -= self.s
 
-class Autobus(Transport):
-    def __init__(self, name, max_speed, mileage):
-        super().__init__(name, max_speed, mileage)
+    def go_right(self):
+        self.x += self.s
 
-    def seating_capacity(self, capacity=50):
-        return f"Вместимость одного автобуса {self.name}: {capacity} пассажиров"
+    def go_left(self):
+        self.x -= self.s
 
-autobus = Autobus("Renaul Logan", 150, 15)
-print(autobus.seating_capacity())
+    def evolve(self):
+        self.s += 1
+
+    def degrade(self):
+        self.s -= 1
+
+    def count_moves(self, x2, y2):
+        return abs(x2 - self.x) + abs(y2 - self.y)
+
+    def print_coords(self):
+        print(f"X: {self.x}", f"Y: {self.y}")
+
+turtle = Turtle(0, 0, 1)
+turtle.evolve()
+turtle.go_up()
+turtle.print_coords()
+print(turtle.count_moves(5, 10))
