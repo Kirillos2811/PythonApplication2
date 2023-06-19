590dda42b39b5202737dc751d12876b9b4e90419
Kirillos2811 <kirill.sesorov@gmail.com>
2023-06-19 23:12:02 +0300

    Lesson 16 task 2

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -23,8 +23,24 @@ class Turtle:
     def degrade(self):
         self.s -= 1
 
+    @staticmethod
+    def recursive_seeking(x, y, x2, y2, s, count):
+        if x == x2 and y == y2:
+            return count
+        if x2 > x:
+            return Turtle.recursive_seeking(x + s, y, x2, y2, s, count + 1)
+        elif x2 < x:
+            return Turtle.recursive_seeking(x - s, y, x2, y2, s, count + 1)
+        elif y2 > y:
+            return Turtle.recursive_seeking(x, y + s, x2, y2, s, count + 1)
+        elif y2 < y:
+            return Turtle.recursive_seeking(x, y - s, x2, y2, s, count + 1)
+
     def count_moves(self, x2, y2):
-        return abs(x2 - self.x) + abs(y2 - self.y)
+        try:
+            return self.recursive_seeking(self.x, self.y, x2, y2, self.s, 0)
+        except RecursionError:
+            return "Невозможно добраться до цели"
 
     def print_coords(self):
         print(f"X: {self.x}", f"Y: {self.y}")
@@ -33,4 +49,4 @@ turtle = Turtle(0, 0, 1)
 turtle.evolve()
 turtle.go_up()
 turtle.print_coords()
-print(turtle.count_moves(5, 10))
+print(turtle.count_moves(6, 10))
